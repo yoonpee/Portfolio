828b0a90caef5958b1e2dc4976790f06b1bf187b
yoonpee <73012145+yoonpee@users.noreply.github.com>
2021-05-02 13:30:58 +0900

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.querySelector('.toggle');
-const nav = document.querySelector('.nav')
-
-toggle.addEventListener('click', ()=>{
-    toggle.classList.toggle('on');
-    nav.classList.toggle('on');
-})
